c5a3298db90efd92c1e4392b41893b5ccf48eead
SANGWOOSEE <washble@devdeopt.co.kr>
2024-06-03 00:30:09 +0900

    Chagne Path

--- assets/js/service-worker.js
+++ assets/js/service-worker.js
@@ -5,16 +5,16 @@ self.addEventListener('install', event => {
     event.waitUntil(
         caches.open('static-v1').then(cache => {
             return cache.addAll([
-                '/',
-                '/index.html',
-                '/assets/css/style.css',
-                '/assets/css/camera.css',
-                '/assets/vendor/bootstrap/css/bootstrap.min.css',
-                '/assets/vendor/bootstrap-icons/bootstrap-icons.css',
-                '/assets/vendor/boxicons/css/boxicons.min.css',
-                '/assets/vendor/glightbox/css/glightbox.min.css',
-                '/assets/vendor/swiper/swiper-bundle.min.css',
-                '/assets/js/camera.js',
+                './',
+                './index.html',
+                './assets/css/style.css',
+                './assets/css/camera.css',
+                './assets/vendor/bootstrap/css/bootstrap.min.css',
+                './assets/vendor/bootstrap-icons/bootstrap-icons.css',
+                './assets/vendor/boxicons/css/boxicons.min.css',
+                './assets/vendor/glightbox/css/glightbox.min.css',
+                './assets/vendor/swiper/swiper-bundle.min.css',
+                './assets/js/camera.js',
             ]);
         })
     );
