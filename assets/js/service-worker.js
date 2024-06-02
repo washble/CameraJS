64794f43dd15a95f89ee9877a654799164ea348d
SANGWOOSEE <washble@devdeopt.co.kr>
2024-06-03 00:18:22 +0900

    Change paht

--- assets/js/service-worker.js
+++ assets/js/service-worker.js
@@ -4,18 +4,18 @@ self.addEventListener('install', event => {
     // 캐싱할 파일 목록을 지정합니다.
     event.waitUntil(
         caches.open('static-v1').then(cache => {
-        return cache.addAll([
-            '/',
-            '/index.html',
-            '/assets/css/style.css',
-            '/assets/css/camera.css',
-            'assets/vendor/bootstrap/css/bootstrap.min.css',
-            'assets/vendor/bootstrap-icons/bootstrap-icons.css',
-            'assets/vendor/boxicons/css/boxicons.min.css',
-            'assets/vendor/glightbox/css/glightbox.min.css',
-            'assets/vendor/swiper/swiper-bundle.min.css',
-            '/assets/js/camera.js',
-        ]);
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/assets/css/style.css',
+                '/assets/css/camera.css',
+                '/assets/vendor/bootstrap/css/bootstrap.min.css',
+                '/assets/vendor/bootstrap-icons/bootstrap-icons.css',
+                '/assets/vendor/boxicons/css/boxicons.min.css',
+                '/assets/vendor/glightbox/css/glightbox.min.css',
+                '/assets/vendor/swiper/swiper-bundle.min.css',
+                '/assets/js/camera.js',
+            ]);
         })
     );
 });
