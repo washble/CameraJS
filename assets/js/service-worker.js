de838ef88364da07fda32582b4e322fc92e0b83a
SANGWOOSEE <washble@devdeopt.co.kr>
2024-06-02 21:06:19 +0900

    Apply SWA

---
+++ assets/js/service-worker.js
@@ -0,0 +1,9 @@
+
+// service-worker.js
+self.addEventListener('install', event => {
+    console.log('Service worker installed');
+});
+
+self.addEventListener('fetch', event => {
+    console.log('Fetch intercepted for:', event.request.url);
+});
